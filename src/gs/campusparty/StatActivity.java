690e374ec82f8ccf209cc422265694bc598f1b37
grill <gabriel.grill@aon.at>
2012-08-23 02:22:07 +0200

    ich kann das auch

--- src/gs/campusparty/StatActivity.java
+++ src/gs/campusparty/StatActivity.java
@@ -2,6 +2,7 @@ package gs.campusparty;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StatActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +10,19 @@ public class StatActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Field f = Field.getSingleton();
+        f.ghosts[0].label = (TextView) findViewById(R.id.txtfield0time);
+        f.ghosts[1].label = (TextView) findViewById(R.id.txtfield1time);
+        f.ghosts[2].label = (TextView) findViewById(R.id.txtfield2time);
+        f.ghosts[3].label = (TextView) findViewById(R.id.txtfield3time);
+        f.ghosts[4].label = (TextView) findViewById(R.id.txtfield4time);
+        f.ghosts[5].label = (TextView) findViewById(R.id.txtfield5time);
+        f.ghosts[6].label = (TextView) findViewById(R.id.txtfield6time);
+        f.ghosts[7].label = (TextView) findViewById(R.id.txtfield7time);
+        f.ghosts[8].label = (TextView) findViewById(R.id.txtfield8time);
+        
+        Timer t = new Timer();
+        t.start();
     }
 }
